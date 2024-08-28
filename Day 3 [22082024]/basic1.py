534b5ab7fa07d9b53fd84d5016b755a97b925667
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-08-28 08:37:02 +0530

    Add files via upload

---
+++ Day 3 [22082024]/basic1.py
@@ -0,0 +1,98 @@
+#comments
+'''
+print("abc")
+'''
+'''
+print("abc")
+print("abc")
+print("abc")
+print("abc")
+print("abc")
+'''
+#print("abc")
+#Key Words
+'''
+import keyword
+print(keyword.kwlist)
+'''
+#Identifiers
+'''
+a = 100
+b = 200
+c = 100
+print(id(a))
+print(id(b))
+print(id(c))
+'''
+#Variables
+#Naming Rules
+'''
+a = 10
+print(a)
+_10 = 100
+print(_10)
+a123 = 2024
+print(a123)
+'''
+#Assignment and Reassignment
+'''
+a = 100
+print(a)
+a = 200
+print(a)
+'''
+#Multiple Assignment
+'''
+a,b,c,d = 10,20,30,40
+print(a)
+print(b)
+print(c)
+print(d)
+x=y=z=2020
+print(x)
+print(y)
+print(z)
+'''
+#Swapping
+'''
+a = 1010
+b = 2020
+print(a,b)
+a,b = b,a
+print(a,b)
+'''
+#Deleting Variable
+'''
+a = 1010
+print(a)
+del a
+print(a)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
