f8637ddd330edf4513dc09c7db2ec6986573041e
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-08-31 16:39:13 +0530

    Add files via upload

---
+++ Day 7 [31082024] - Test 2 execution/execution.py
@@ -0,0 +1,36 @@
+#Question1
+'''
+a = [10,20,30,40,20,56,20]
+y = 2
+print(a.count(a[y]))
+'''
+#Question2
+'''
+s = "HELLOWORLD"
+x = "O"
+start = s.find(x)
+end = s.rfind(x) + 1
+z = s[start:end:1]
+print(z,len(z))
+'''
+#QUESTION3
+'''
+append()
+extend()
+insert()
+'''
+#QUESTION4
+'''
+Any 5 methods
+'''
+
+
+
+
+
+
+
+
+
+
+
